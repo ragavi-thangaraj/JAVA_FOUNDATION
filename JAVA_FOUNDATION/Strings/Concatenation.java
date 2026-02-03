2b9f0b9bdeef4728e244aeb29044adfb85a4e932
ragavi <ragavithangaraj09@gmail.com>
2026-02-03 12:12:21 +0530

    chore: reverse a string

--- JAVA_FOUNDATION/Strings/Concatenation.java
+++ JAVA_FOUNDATION/Strings/Concatenation.java
@@ -1,3 +1,5 @@
+package Strings;
+
 public class Concatenation 
 {
     public static void main(String[] args) {
---
+++ JAVA_FOUNDATION/Strings/Reverse.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the original string: "+" ");
+        String s=sc.next();
+        String s1= new String();
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            s1+=s.charAt(i);
+        }
+        System.out.println("Original String: "+s);
+        System.out.println("Reversed String: "+s1);
+    }
+    
+}
+
+/*
+Enter the original string:  athik
+Original String: athik
+Reversed String: kihta
+*/
